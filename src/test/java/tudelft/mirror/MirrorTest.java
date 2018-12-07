e15c301e0d6a5ffa88b911660e0c9c6e9743f9e8
Konstantin Zhulidov <kzhulidov@gmail.com>
2018-12-07 22:56:10 +0100

    Week 4

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,71 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+    @Test
+    public void noChars() {
+
+        Mirror mi = new Mirror();
+
+        Assertions.assertEquals("", mi.mirrorEnds(""));
+    }
+
+    @Test
+    public void oneChar() {
+
+        Mirror mi = new Mirror();
+
+        Assertions.assertEquals("a", mi.mirrorEnds("a"));
+    }
+
+    @Test
+    public void twoChars() {
+
+        Mirror mi = new Mirror();
+
+        Assertions.assertEquals("", mi.mirrorEnds("ab"));
+    }
+
+    @Test
+    public void fewChars() {
+
+        Mirror mi = new Mirror();
+
+        Assertions.assertEquals("ab", mi.mirrorEnds("abba"));
+    }
+
+    @Test
+    public void evenMoreChars() {
+
+        Mirror mi = new Mirror();
+
+        Assertions.assertEquals("a", mi.mirrorEnds("abca"));
+    }
+
+    @Test
+    public void oddChars() {
+
+        Mirror mi = new Mirror();
+
+        Assertions.assertEquals("aba", mi.mirrorEnds("aba"));
+    }
+
+    @Test
+    public void strangeChars() {
+
+        Mirror mi = new Mirror();
+
+        Assertions.assertEquals("ab", mi.mirrorEnds("abcdba"));
+    }
+
+    @Test
+    public void wowChars() {
+
+        Mirror mi = new Mirror();
+
+        Assertions.assertEquals("ab", mi.mirrorEnds("abcdeba"));
+    }
 }
